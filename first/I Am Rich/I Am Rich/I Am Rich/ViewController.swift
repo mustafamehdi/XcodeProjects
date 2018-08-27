5a2970e0a103b860fea8859915b44710c444ebfb
Mustafa <mustafa.5657@gmail.com>
2018-08-26 23:00:30 -0400

    myxocde learning
    my xocode learning prokjects

---
+++ first/I Am Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mustafa Mehdi on 2018-08-25.
+//  Copyright © 2018 Mustafa Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ first/learn_basics1/learn_basics1/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  learn_basics1
+//
+//  Created by Mustafa Mehdi on 2018-08-19.
+//  Copyright © 2018 Mustafa Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+  
+    
+}
+
